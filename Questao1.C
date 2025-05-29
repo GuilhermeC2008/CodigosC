213a351b900adea50e1ad3e513a445252df119d0
GuilhermeC2008 <164903556+GuilhermeC2008@users.noreply.github.com>
2025-05-29 11:52:48 -0300

    Create Questao1.C

---
+++ Questao1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+int n1, n2, soma;
+
+printf("Insira o primeiro número: \n");
+scanf("%d", &n1);
+
+printf("Insira o segundo número: \n");
+scanf("%d", &n2);
+
+soma = (n1+n2);
+
+printf("A soma dos números é: \n %d", soma);
+    return 0;
+}
